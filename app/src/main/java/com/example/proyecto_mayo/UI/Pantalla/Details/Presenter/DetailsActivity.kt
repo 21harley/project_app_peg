7d8e344b74289171856b079c0bd47da60a3cf1b7
MavRial <108558141+MavRial@users.noreply.github.com>
2024-05-13 15:02:51 -0300

    add Home Layout

--- app/src/main/java/com/example/proyecto_mayo/UI/Pantalla/Details/Presenter/DetailsActivity.kt
+++ app/src/main/java/com/example/proyecto_mayo/UI/Pantalla/Details/Presenter/DetailsActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.proyecto_mayo.R
 
 class DetailsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
