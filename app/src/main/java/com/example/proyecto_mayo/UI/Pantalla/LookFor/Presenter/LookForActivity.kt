7e3487084a34666eab8f5b8ff6af712616c72f95
MavRial <108558141+MavRial@users.noreply.github.com>
2024-05-12 22:37:28 -0300

    add files

---
+++ app/src/main/java/com/example/proyecto_mayo/UI/Pantalla/LookFor/Presenter/LookForActivity.kt
@@ -0,0 +1,21 @@
+package com.example.proyecto_mayo.UI.Pantalla.LookFor.Presenter
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.proyecto_mayo.R
+
+class LookForActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_look_for)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
