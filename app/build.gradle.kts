97f5d888164f88e25722cba13886dc8493a34b73
MavRial <108558141+MavRial@users.noreply.github.com>
2024-05-13 20:05:43 -0300

    add views

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,7 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.retrofit)
@@ -50,6 +49,11 @@ dependencies {
     implementation(libs.glide)
     implementation (libs.androidx.lifecycle.viewmodel.ktx)
     implementation (libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.cardview)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
